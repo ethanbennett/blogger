f7039df01b1369be4b3d16a14ce9990a678a1d35
Ethan Bennett <ebennett19@gmail.com>
2016-12-11 15:59:41 -0700

    Add edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,10 @@ class ArticlesController < ApplicationController
     redirect_to action: "index"
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   private
 
   def article_params
